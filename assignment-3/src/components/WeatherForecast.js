695a2d6791d5634c5f246981b45b583e253e838c
aliihsanakcay <alihsanakcayy@gmail.com>
2022-02-13 12:25:57 +0300

    assignment-3

--- assignment-3/src/components/WeatherForecast.js
+++ assignment-3/src/components/WeatherForecast.js
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import useGet from "../hooks/useGet";
 import { useGlobal } from "../context/GlobalContext";
 import "../App.css"
+import config from "../utils/config.json"
 
 function DailyWeather() {
     const { selectedCity } = useGlobal();
-    const { responseData, loading, error, get } = useGet();
+    const { responseData, get } = useGet();
     let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
     useEffect(() => {
         const getData = async () => {
-            const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${selectedCity.latitude}&lon=${selectedCity.longitude}&exclude=hourly,minutely,current&appid=0df58d30ce9fd51312fbcc94db5eb2b2`;
+            const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${selectedCity.latitude}&lon=${selectedCity.longitude}&exclude=hourly,minutely,current&appid=${config.openWeatherAPIKey}`;
 
             await get(url);
         }
@@ -30,7 +31,7 @@ function DailyWeather() {
                     return (
                         <div key={index} className={index === 0 ? "today" : "otherDays"}>
                             <div>{days[date.getDay()]}</div>
-                            <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`} />
+                            <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`} alt={"weatherIcon"} />
                             <div>{Math.round(day.temp.max - 273.15)}&#8451;&#160;&#160;&#160;{Math.round(day.temp.min - 273.15)}&#8451;</div>
                         </div>
                     );
